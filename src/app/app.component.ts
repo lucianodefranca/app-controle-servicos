1949f52b48252a391ecc1977756c871aeabc3261
luciano de franca <lucianodefranca86@gmail.com>
2022-02-21 19:46:14 -0300

    Componente sidebar, funcao no botao menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   title = 'app-clientes';
+
+  ngAfterViewInit(): void {
+    (function ($) {
+      'use strict';
+
+      var path = window.location.href;
+      $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function () {
+        if (window.location.href === path) {
+          $(this).addClass('active');
+        }
+      });
+
+      // Toggle the side navigation
+      $('#sidebarToggle').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('sb-sidenav-toggled');
+      });
+    })(jQuery);
+  }
 }
