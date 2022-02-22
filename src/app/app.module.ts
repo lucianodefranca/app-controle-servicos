7f8bcf2a23b7ed78698a711807c43b18ea2d894a
luciano de franca <lucianodefranca86@gmail.com>
2022-02-21 22:06:37 -0300

    Componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
